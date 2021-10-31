3e64e8fb58e4d5d9b127149fa834579039e8083f
Alex Godfrey <agod@dhcp-vl2042-23678.redrover.cornell.edu>
2021-10-31 19:17:26 -0400

    small fix to enable no cloud flow fetching

--- functions/routes/flows.js
+++ functions/routes/flows.js
@@ -5,12 +5,24 @@
 
  module.exports = function (app) {
 
-    //digital flows
-    app.get("/api/flows/:apporcloud/:cloud/:tasks", async (req, res) => {
+    //digital writing flows with cloud
+    app.get("/api/flows/cloud/:apporcloud/:cloud/:tasks", async (req, res) => {
         try {
             const { apporcloud, cloud, tasks } = req.params;
             const query = await pool.query("SELECT * FROM flowlist WHERE $1 = any (tags) AND $2 = any (tags) AND $3 = any (tags) ORDER BY flowlist.difficulty != 'Average', flowlist.difficulty != 'Hard', id asc ", 
             [apporcloud, cloud, tasks]);
+            res.json(query.rows);
+        } catch (err) {
+            console.error(err.message);
+        }
+    })
+
+    //digital writing flows without cloud
+    app.get("/api/flows/nocloud/:apporcloud/:tasks", async (req, res) => {
+        try {
+            const { apporcloud, tasks } = req.params;
+            const query = await pool.query("SELECT * FROM flowlist WHERE $1 = any (tags) AND $2 = any (tags) ORDER BY flowlist.difficulty != 'Average', flowlist.difficulty != 'Hard', id asc", 
+            [apporcloud, tasks]);
             res.json(query.rows);
         } catch (err) {
             console.error(err.message);
