000a649050c4a45ffc23f3355824e919f5bd102f
Alex Godfrey <alexjamesgodfrey@gmail.com>
2021-11-14 23:16:48 -0500

    function to create database

--- server/bin/index.js
+++ server/bin/index.js
@@ -2,6 +2,7 @@
 const notionPage = require('../notion/page')
 const notion = require('../notion/notion.js')
 
-//notion.getAccessToken('7199ac34-f964-4504-88d4-f5829632f753', '5')
+//notion.getAccessToken('5bea7791-272f-4f76-a63b-b6bfba4a23d8', '5')
 //notionPage.createPage('secret_DoCxqCrdDosUz5Fu8SGT3sRTEqsw9Q0jg5b5hZQ7ckU')
-notionPage.listPages('secret_DoCxqCrdDosUz5Fu8SGT3sRTEqsw9Q0jg5b5hZQ7ckU')
+//notionPage.listPages('secret_GuT28RSGQVKBbfDeqguGaeyKdtu8tOyOaioONxaSzAB')
+notionPage.formPage('secret_GuT28RSGQVKBbfDeqguGaeyKdtu8tOyOaioONxaSzAB', '8b6815e0-b4e7-4eca-8b8f-973b00148ae4', ['MATH1120', 'CS1110', 'ASIAN2299'])
--- server/notion/page.js
+++ server/notion/page.js
@@ -1,13 +1,85 @@
 const { Client } = require('@notionhq/client');
 
 /**
- * Returns a list of all page id's users Notion
+ * Returns a list of all non-archived page id's users Notion
  * @param {String} access_token the users Notion access token.
  */
 const listPages = async (access_token) => {
     const notion = new Client({ auth: access_token });
     const response = await notion.search();
-    return response.results.map(page => page.id)
+    console.log(response)
+    // return response.results.map(page => {
+    //     if (page.archived == false){ 
+    //         return page.id
+    //     }
+    // })
+}
+
+/**
+ * Function formPage (1) clears the desired page of all blocks (2) creates a 
+ * database in the page. 
+ * @param {String} access_token the user's Notion access token
+ * @param {String} page_id the id of the page
+ * @param {Array} classes_array array of the user's classes
+ */
+const formPage = async (access_token, page_id, classes_array) => {
+    const notion = new Client({ auth: access_token });
+
+    //delete all page content
+    const pageBlocks = await notion.blocks.children.list({
+        block_id: page_id,
+        page_size: 100,
+    });
+    console.log(pageBlocks)
+    const pageBlockList = pageBlocks.results.map(block => block.id)
+    console.log(pageBlockList)
+    for (let i=0; i<pageBlockList.length; i++) {
+        const response = await notion.blocks.delete({
+            block_id: pageBlockList[i],
+        });
+    }
+
+    //creates an object for each class for classes dropdown
+    const colors = ["gray", "brown", "orange", "yellow", "green", "blue", "purple", "pink", "red"]
+    const classes = classes_array.map(c => {
+        return {
+            name: c,
+            color: colors[Math.floor(Math.random() * 7)]
+        }
+    })
+
+    //create a database in page
+    const createDatabase = await notion.databases.create({
+        parent: {
+            type: "page_id",
+            page_id: page_id 
+        },
+        title: [
+            {
+                type: "text",
+                text: {
+                    "content": "MASTER DATABASE",
+                    "link": null
+                }
+            }
+        ],
+        properties: {
+            "Name": {
+                "title": {}
+            },
+            "Date": {
+                "date": {}
+            },
+            "Class": {
+                "multi_select": {
+                    "options": classes
+                }
+            },
+        }
+    });
+
+
+
 }
 
 /**
@@ -103,4 +175,4 @@ const createPage = async (access_token) => {
     console.log(response);
 }
 
-module.exports = {createPage, listPages}
+module.exports = {listPages, createPage, formPage}
