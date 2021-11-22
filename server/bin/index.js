65f1f8c7f687d26ace1c4bf456bf783628ba397a
Alex Godfrey <alexjamesgodfrey@gmail.com>
2021-11-22 16:36:20 -0500

    welcome page now works

--- server/bin/index.js
+++ server/bin/index.js
@@ -5,14 +5,20 @@ const notion = require('../notion/notion.js')
 //notion.getAccessToken('315220ee-aabc-46b1-bf64-810dc6009b59', '5')
 //notionPage.createPage('secret_NNA9XZlSbctguV8HiuC9aq6HiV659rK3f8HmuXB8Ydn')
 //notionPage.listPages('secret_NNA9XZlSbctguV8HiuC9aq6HiV659rK3f8HmuXB8Ydn')
-//notionPage.formParent('secret_NNA9XZlSbctguV8HiuC9aq6HiV659rK3f8HmuXB8Ydn', 'cde4d8d3-8cfa-461e-ac1d-2d3725053143', ['MATH1120', 'CS1110', 'ASIAN2299'], 5)
-notionPage.createPageInDatabase(
-    'secret_NNA9XZlSbctguV8HiuC9aq6HiV659rK3f8HmuXB8Ydn', 
-    '174bea26-f92e-4d82-b30f-9924d0b807e7',
-    'L1 WELCOME TO STUDYFLOW',
-    new Date().toISOString().substring(0, 10),
-    'MATH1120'
-)
-//notionPage.createWelcomePage('secret_NNA9XZlSbctguV8HiuC9aq6HiV659rK3f8HmuXB8Ydn', 'cde4d8d3-8cfa-461e-ac1d-2d3725053143')
+notionPage.formParent('secret_NNA9XZlSbctguV8HiuC9aq6HiV659rK3f8HmuXB8Ydn', 'cde4d8d3-8cfa-461e-ac1d-2d3725053143', ['MATH1120', 'CS1110', 'ASIAN2299'], 5)
+// const d = new Date()
+// d.setHours(d.getHours() -1)
+// notionPage.createPageInDatabase(
+//     'secret_NNA9XZlSbctguV8HiuC9aq6HiV659rK3f8HmuXB8Ydn', 
+//     '174bea26-f92e-4d82-b30f-9924d0b807e7',
+//     'L1 WELCOME TO STUDYFLOW',
+//     d.toISOString().substring(0, 10),
+//     'MATH1120'
+// )
+// notionPage.createWelcomePage(
+//     'secret_NNA9XZlSbctguV8HiuC9aq6HiV659rK3f8HmuXB8Ydn', 
+//     '7effd6a9-be9b-4644-9ac9-25672bb8ed5d',
+//     d.toISOString().substring(0, 10),
+// )
 
 
--- server/notion/page.js
+++ server/notion/page.js
@@ -1,5 +1,6 @@
-const { Client, LogLevel } = require('@notionhq/client');
+const { Client } = require('@notionhq/client');
 require("dotenv").config();
+const moment = require('moment')
 
 /**
  * Returns a list of all non-archived page id's users Notion
@@ -26,7 +27,7 @@ const listPages = async (access_token) => {
  * @param {String} user_id the user's primary key from users database
  */
 const createMasterDatabase = async (access_token, page_id, classes_array, user_id) => {
-    console.log('Creating master database for user ' + access_token)
+    console.log(`[${moment().format('L')} ${moment().format('LTS')}] Creating master database for user ${access_token}`)
     const notion = new Client({ auth: access_token });
 
     const colors = ["gray", "brown", "orange", "yellow", "green", "blue", "purple", "pink", "red"]
@@ -89,9 +90,6 @@ const createMasterDatabase = async (access_token, page_id, classes_array, user_i
             },
         }
     });
-    console.log('Database information:')
-    console.log(JSON.stringify(createDatabase))
-    console.log(user_id)
     await fetch(`${process.env.BASE_REQUEST_URL}/api/notioninformation/database/${user_id}`, {
         method: "PUT",
         headers: {
@@ -99,6 +97,11 @@ const createMasterDatabase = async (access_token, page_id, classes_array, user_i
         },
         body: JSON.stringify(createDatabase)
     })
+
+    //create welcome page
+    const d = new Date()
+    d.setHours(d.getHours() -1)
+    createWelcomePage(access_token, createDatabase.id, d.toISOString().substring(0, 10))
 }
 
 
@@ -111,6 +114,7 @@ const createMasterDatabase = async (access_token, page_id, classes_array, user_i
  * @param {String} user_id the user's primary key from the users database
  */
 const formParent = async (access_token, page_id, classes_array, user_id) => {
+    console.log(`[${moment().format('L')} ${moment().format('LTS')}] Forming parent for ${access_token}`)
     const notion = new Client({ auth: access_token });
 
     //delete all page content
@@ -125,25 +129,440 @@ const formParent = async (access_token, page_id, classes_array, user_id) => {
         });
     }
 
+    const response = await notion.blocks.children.append({
+        block_id: page_id,
+        children: [
+          {
+            object: 'block',
+            type: 'callout',
+            "callout": {
+                "text": [
+                    {
+                        "type": "text",
+                        "text": {
+                            "content": "Click below to view all your notes",
+                            "link": null
+                        },
+                        "plain_text": "Click below to view all your notes",
+                        "href": null
+                    }
+                ],
+                "icon": {
+                    "type": "emoji",
+                    "emoji": "💡"
+                }
+            }
+          },
+        ],
+      });
+
     createMasterDatabase(access_token, page_id, classes_array, user_id)
 }
 
-const createWelcomePage = async (access_token, page_id) => {
-    await fetch('https://api.notion.com/v1/pages', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${access_token}`,
-            "Notion-Version": "2021-08-16"
+const createWelcomePage = async (access_token, database_id, date) => {
+    console.log(`[${moment().format('L')} ${moment().format('LTS')}] Creating welcome page for ${access_token}`)
+
+    const notion = new Client({ auth: access_token });
+    
+    const response = await notion.pages.create({
+        parent: {
+            database_id: database_id,
         },
-        body: JSON.stringify({
-            parent: {
-                page_id: page_id,
+        icon: {
+            type: "emoji",
+            emoji: "🚨"
+        },
+        properties: {
+            Name: {
+                title: [
+                    {
+                        text: {
+                            content: "WELCOME TO STUDYFLOW",
+                        },
+                    },
+                ],
+            },
+            Date: {
+                date: 
+                    {
+                        start: date
+                    }
             }
-        })
-    })
-        .then(response => response.json())
-        .then(data => console.log(data))
+        },
+        children: [
+            {
+                object: 'block',
+                type: 'bulleted_list_item',
+                bulleted_list_item: {
+                    text: [
+                        {
+                            type: "text",
+                            text: {
+                                content: `[${new Date().toLocaleString()}] Entry `,
+                            }
+                        },
+                        {
+                            type: "text",
+                            text: {
+                                content: "WELCOME TO STUDYFLOW ",
+                            }, 
+                            annotations: {
+                                italic: true
+                            }
+                        },
+                        {
+                            type: "text",
+                            text: {
+                                content: `created by `,
+                            }
+                        },
+                        {
+                            type: "text",
+                            text: {
+                                content: `studyflow.ai`,
+                                link: {
+                                    url: "https://studyflow.ai"
+                                }
+                            },
+                            annotations: {
+                                bold: true
+                            }
+                        },
+                        {
+                            type: "text",
+                            text: {
+                                content: `.`,
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                object: 'block',
+                type: "bulleted_list_item",
+                bulleted_list_item: {
+                    text: [
+                        {
+                            type: "text",
+                            text: {
+                                content: 'Scroll up to view page properties: create questions or review based on '
+                            }
+                        },
+                        {
+                            type: "text",
+                            text: {
+                                content: 'Next Task '
+                            },
+                            annotations: {
+                                bold: true
+                            }
+                        },
+                        {
+                            type: "text",
+                            text: {
+                                content: 'property.'
+                            }
+                        },
+                    ]
+                }
+            },
+            {
+                object: 'block',
+                type: "bulleted_list_item",
+                bulleted_list_item: {
+                    text: [
+                        {
+                            type: "text",
+                            text: {
+                                content: 'Please view the '
+                            }
+                        },
+                        {
+                            type: "text",
+                            text: {
+                                content: 'FAQ ',
+                                link: {
+                                    url: 'https://studyflow.ai'
+                                }
+                            },
+                            annotations: {
+                                bold: true
+                            }
+                        },
+                        {
+                            type: "text",
+                            text: {
+                                content: 'or '
+                            }
+                        },
+                        {
+                            type: "text",
+                            text: {
+                                content: 'contact us',
+                                link: {
+                                    url: 'https://studyflow.ai'
+                                }
+                            },
+                            annotations: {
+                                bold: true
+                            }
+                        },
+                        {
+                            type: "text",
+                            text: {
+                                content: ' with questions, and enjoy studying efficiently!'
+                            }
+                        },
+                    ]
+                }
+            },
+            {
+                object: 'block',
+                type: "heading_2",
+                heading_2: {
+                    text: [
+                        {
+                            type: "text",
+                            text: {
+                                "content": "Active Recall Questions",
+                                "link": null
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                object: 'block',
+                type: "heading_3",
+                heading_3: {
+                    text: [
+                        {
+                            type: "text",
+                            text: {
+                                "content": "SECTION 1: A THANK YOU",
+                                "link": null
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                object: 'block',
+                type: "paragraph",
+                paragraph: {
+                    text: [
+                        {
+                            type: "text",
+                            text: {
+                                "content": ("Alex from Studyflow here 👋. Thanks so much for trying out Studyflow--" + 
+                                "college is a busy time, and you just took a great step to making it a better experience. " + 
+                                "I'm confident that we will save you time and help you learn even more than you already are. " +
+                                "If you find a bug or need any assistance at all, please don't hesitate to contact us with the above link.\n" +
+                                "Best of luck!"),
+                                "link": null
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                object: 'block',
+                type: "heading_3",
+                heading_3: {
+                    text: [
+                        {
+                            type: "text",
+                            text: {
+                                "content": "SECTION 2: REVIEW QUESTIONS TUTORIAL",
+                                "link": null
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                object: 'block',
+                type: "paragraph",
+                paragraph: {
+                    text: [
+                        {
+                            type: "text",
+                            text: {
+                                "content": ('On the day you take notes, you should create review questions. These are known as "active recall quetions" and answering them periodically helps you learn quickly ' +
+                                "Below are the three possible question types to ask in your toggles. " +
+                                "But please experiment! As the Studyflow community grows, more efficient ways of studying will be discovered--" +
+                                "and we will all benefit from them."),
+                                "link": null
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                object: 'block',
+                type: 'toggle',
+                toggle: {
+                    text: [
+                        {
+                            type: 'text',
+                            text: {
+                                content: 'TYPE 1: FILL IN THE BLANK -- There are ___ ______ants in the world'
+                        }
+                    }],
+                    "children":[{
+                        type: "paragraph",
+                        paragraph: {
+                            text: [
+                                {
+                                    type: "text",
+                                    text: {
+                                        "content": ("one quadrillion"),
+                                        "link": {
+                                            url: 'https://www.combatbugs.com/en/pest-identification/bug-files/how-many-ants-are-there-in-the-world'
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }]
+                }
+            },
+            {
+                object: 'block',
+                type: 'toggle',
+                toggle: {
+                    text: [
+                        {
+                            type: 'text',
+                            text: {
+                                content: 'TYPE 2: SIMPLE QUESTION -- What is the lifespan of a black garden ant?'
+                        }
+                    }],
+                    "children":[{
+                        type: "paragraph",
+                        paragraph: {
+                            text: [
+                                {
+                                    type: "text",
+                                    text: {
+                                        "content": ("4 years"),
+                                        "link": {
+                                            url: 'https://en.wikipedia.org/wiki/Black_garden_ant'
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }]
+                }
+            },
+            {
+                object: 'block',
+                type: 'toggle',
+                toggle: {
+                    text: [
+                        {
+                            type: 'text',
+                            text: {
+                                content: 'TYPE 3: LIST QUESTION -- What are the three most common ant types?'
+                        }
+                    }],
+                    "children":[{
+                        type: "paragraph",
+                        paragraph: {
+                            text: [
+                                {
+                                    type: "text",
+                                    text: {
+                                        "content": ("1. Carpenter Ants 2. Odorous House Ants 3. Pavement Ants"),
+                                        "link": {
+                                            url: 'https://www.pestworld.org/news-hub/guest-columnists/the-three-most-common-ant-species/'
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }]
+                }
+            },
+            {
+                object: 'block',
+                type: "heading_3",
+                heading_3: {
+                    text: [
+                        {
+                            type: "text",
+                            text: {
+                                "content": "SECTION 3: REVIEW PERIODICALLY",
+                                "link": null
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                object: 'block',
+                type: "paragraph",
+                paragraph: {
+                    text: [
+                        {
+                            type: "text",
+                            text: {
+                                "content": ('After you create your review questions, you will be prompted periodically (1 day, 3 days, 7 days, and 30 days after creation) ' +
+                                'to answer your questions. Refer to the page properties at the top of the page. The "Next Task" property will tell you when to review next. After review, check the RX (e.g., R1 for first review) box. ' +
+                                "The Next Task property will automatically generate your next review date. We suggest you check your MASTER DATABASE every day for review."),
+                                "link": null
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                object: 'block',
+                type: "heading_3",
+                heading_3: {
+                    text: [
+                        {
+                            type: "text",
+                            text: {
+                                "content": "SECTION 4: PRACTICE PROBLEMS",
+                                "link": null
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                object: 'block',
+                type: "paragraph",
+                paragraph: {
+                    text: [
+                        {
+                            type: "text",
+                            text: {
+                                "content": ('For classes focused on computation, like Calculus, ' + 
+                                'it is important to do practice problems. We recommend you create checkboxes to complete practice problems during each review.'),
+                                "link": null
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                "type": "to_do",
+                "to_do": {
+                  "text": [{
+                    "type": "text",
+                    "text": {
+                      "content": "3 practice problems from section 6.2",
+                      "link": null
+                    }
+                  }],
+                  "checked": false,
+                }
+              }
+          ],
+    });
 }
 
 
