550754278e5828317d1a944649f791727a19dd92
Alex Godfrey <alexjamesgodfrey@gmail.com>
2021-12-20 15:47:08 -0500

    added storage rules

--- client/src/components/Home/ProfileCards.js
+++ client/src/components/Home/ProfileCards.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from 'react'
 import { useAuth } from '../../contexts/AuthContext'
 import { Checkmark } from 'react-checkmark'
+import Form from 'react-bootstrap/Form'
 import Toggle from 'react-toggle'
 import Card from 'react-bootstrap/Card'
 import Overlay from 'react-bootstrap/Overlay'
@@ -9,6 +10,7 @@ import './ProfileCards.scss'
 
 export default function ProfileCards() {
     const { currentUser, userDB } = useAuth()
+    const [loading, setLoading] = useState(false)
     const primaryColor = '#84cacc'
     const [showPublicAccountTip, setShowPublicAccountTip] = useState(false)
     const publicAccountTarget = useRef(null)
@@ -28,14 +30,23 @@ export default function ProfileCards() {
 
     return (
         <div style={{ margin: '20px 0px' }} className="d-flex justify-content-center"> 
-            <Card style={{ margin: '0px 20px' }}>
-                <Card.Header as="h5" style={{ width: '250px' }}>{currentUser.displayName}</Card.Header>
-                <Card.Text>
-                    Cloud service: {userDB.cloud}
-                </Card.Text>
+            <Card style={{ margin: '0px 20px', width: '300px' }}>
+                <Card.Header as="h5">{currentUser.displayName}</Card.Header>
+                <div style={{ margin: '10px' }}>
+                    <div className="d-flex">
+                        <img style={{ width: '75px', marginBottom: '5px' }} src={currentUser.photoURL} alt="upload profile picture" />
+                        <div>
+                            <p style={{ lineHeight: 0.5 }}>username:</p>
+                            <p style={{ lineHeight: 0.5, marginLeft: '5px' }}>{currentUser.displayName} ✎</p>
+                        </div>
+                        
+                    </div>
+                    
+                    <Form.Control style={{ width: '180px', fontSize: '12px' }} type="file" size="sm" disabled={false} accept="image/*" />
+                </div>
             </Card>
-            <Card style={{ margin: '0px 20px'}}>
-                <Card.Header as="h5" style={{ width: '250px' }}>Connections</Card.Header>
+            <Card style={{ margin: '0px 20px', width: '300px' }}>
+                <Card.Header as="h5">Connections</Card.Header>
                 <Card.Text style={{ margin: '5px 10px 0px 10px' }} className="d-flex align-items-center">
                     <span style={{ width: '180px'}}>Cloud service: {userDB.cloud}</span>
                     <Checkmark size="small" color={primaryColor} />
@@ -53,8 +64,8 @@ export default function ProfileCards() {
                     <span></span>
                 }
             </Card>
-            <Card style={{ margin: '0px 20px' }}>
-                <Card.Header as="h5" style={{ width: '250px' }}>Settings</Card.Header>
+            <Card style={{ margin: '0px 20px', width: '300px' }}>
+                <Card.Header as="h5">Settings</Card.Header>
                 <Card.Text style={{ margin: '5px 10px' }} className="d-flex align-items-center">
                     <label 
                         htmlFor='public-status'
